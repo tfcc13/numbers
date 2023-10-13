a1b386e9d0cbf3d45c10b3aaa635392fcb834404
Tiago <tiagofccarvalho13@gmail.com>
2023-10-13 15:03:54 +0100

    Made Mockito - Ex 6

--- src/test/java/com/aor/numbers/bug8726.java
+++ src/test/java/com/aor/numbers/bug8726.java
@@ -1,30 +1,40 @@
 package com.aor.numbers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
+import org.mockito.Mockito;
+import org.mockito.Mockito.*;
 import java.util.List;
 import java.util.Arrays;
 
 
 public class bug8726 {
     @Test
-    public void fakeBug () {
+    public void fakeBug() {
 
-        class FakeClass implements  GenericListDeduplicator {
+        /*class FakeClass implements  GenericListDeduplicator {
             public List<Integer> deduplicate(List<Integer> list) {
                 return Arrays.asList(1,2,4);
             }
-            
+
         }
+        // USED STUB
 
         GenericListDeduplicator deduplicator = new FakeClass();
         List<Integer> list = Arrays.asList(1,2,4,2);
         ListAggregator aggregator = new ListAggregator();
         int distinct = aggregator.distinct(list, deduplicator);
 
+
+     */
+        GenericListDeduplicator deduplicator = Mockito.mock(GenericListDeduplicator.class);
+        Mockito.when(deduplicator.deduplicate(Mockito.anyList())).thenReturn(Arrays.asList(1, 2, 4));
+
+        List<Integer> list = Arrays.asList(1,2,4,2);
+        ListAggregator aggregator = new ListAggregator();
+        int distinct = aggregator.distinct(list, deduplicator);
+
         Assertions.assertEquals(3, distinct);
-    }
 
     }
-
+}
 
