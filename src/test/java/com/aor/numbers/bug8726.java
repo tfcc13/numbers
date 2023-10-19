ff30d3a39b2cd79678d6b100bd1d078af05c0c31
Tiago <tiagofccarvalho13@gmail.com>
2023-10-19 13:11:22 +0100

    Groovy not compiling, gradle vs java conflicts

--- src/test/java/com/aor/numbers/bug8726.java
+++ src/test/java/com/aor/numbers/bug8726.java
@@ -1,8 +1,10 @@
 package com.aor.numbers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+/*import org.mockito.Mockito;
 import org.mockito.Mockito.*;
+*/
+
 import java.util.List;
 import java.util.Arrays;
 
@@ -11,7 +13,7 @@ public class bug8726 {
     @Test
     public void fakeBug() {
 
-        /*class FakeClass implements  GenericListDeduplicator {
+        class FakeClass implements  GenericListDeduplicator {
             public List<Integer> deduplicate(List<Integer> list) {
                 return Arrays.asList(1,2,4);
             }
@@ -25,14 +27,14 @@ public class bug8726 {
         int distinct = aggregator.distinct(list, deduplicator);
 
 
-     */
+     /*
         GenericListDeduplicator deduplicator = Mockito.mock(GenericListDeduplicator.class);
         Mockito.when(deduplicator.deduplicate(Mockito.anyList())).thenReturn(Arrays.asList(1, 2, 4));
 
         List<Integer> list = Arrays.asList(1,2,4,2);
         ListAggregator aggregator = new ListAggregator();
         int distinct = aggregator.distinct(list, deduplicator);
-
+*/
         Assertions.assertEquals(3, distinct);
 
     }
