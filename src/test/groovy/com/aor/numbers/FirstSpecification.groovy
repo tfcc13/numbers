f3081b09e38188b60ac15c3abfa1c1ee12600a5d
Tiago <tiagofccarvalho13@gmail.com>
2023-10-20 11:50:11 +0100

    Made 11.5

--- src/test/groovy/com/aor/numbers/FirstSpecification.groovy
+++ src/test/groovy/com/aor/numbers/FirstSpecification.groovy
@@ -56,4 +56,14 @@ class FirstSpecification extends  Specification {
     list.size() == 4
 }
 
+    def "numbers to the power of two"(int a, int b, int c) {
+        expect:
+        Math.pow(a, b) == c
+        where:
+        a | b | c
+        1 | 2 | 1
+        2 | 2 | 4
+        3 | 2 | 9
+    }
+
 }
