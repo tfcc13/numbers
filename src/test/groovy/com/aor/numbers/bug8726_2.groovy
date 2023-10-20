41e4d1497ea4a8ea6470ad0baa5e57d7425c9f94
Tiago <tiagofccarvalho13@gmail.com>
2023-10-20 11:56:09 +0100

    Made 11.6 Mock

--- src/test/groovy/com/aor/numbers/bug8726_2.groovy
+++ src/test/groovy/com/aor/numbers/bug8726_2.groovy
@@ -8,11 +8,13 @@
 class bug8726_2 extends Specification{
 
         def "test deduplicate with duplicate elements"() {
+            given:
             def deduplicator = Mock(GenericListDeduplicator)
+            deduplicator.deduplicate(Arrays.asList(1, 2, 4, 2)) >> Arrays.asList(1, 2, 4)
             when:
             def result = deduplicator.deduplicate(Arrays.asList(1, 2, 4, 2))
             then:
-            result == null
+            result == Arrays.asList(1,2,4)
         }
 
     }
